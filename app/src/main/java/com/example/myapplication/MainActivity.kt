efa4e3ac7ce2adf58040dc3dbbd3defe255a405e
Bauerexe <chistidlyadiska@gmail.com>
2024-11-26 19:45:29 +0300

    Delete Key

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
     private val apiBaseUrl = "https://api.giphy.com/"
-    private val apiKey = "KADgXmsMF9LDc8tHx4A2XmVI39da233d" //  https://giphy.com/
+    private val apiKey = "Ваш_Ключ" //  https://giphy.com/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
